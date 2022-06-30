314c8f4fc4023fb4a4140e037f3699d40a376fcb
JungAHyun <66303292+JungAHyun@users.noreply.github.com>
2022-06-30 14:12:38 +0900

    Update 주유소.py

--- greedy/주유소.py
+++ greedy/주유소.py
@@ -22,6 +22,7 @@ j = min_index
 
 if min_momey == money[0]:
     result = money[0]*(sum(lenth))
+
 else:
     while(min_index>1):
         min_momey = money[0]
@@ -34,7 +35,29 @@ else:
         j = min_index
         index.append(j)        
 
-
-index = reversed
-
+lenth_sum = 0
+before_index = 0
+result_money=0
+index = reversed(index)
 print(index)
+for i in index:
+    for j in range(before_index, index[i]):
+        lenth_sum += lenth[j]
+    before_index = index[i]
+    result_money += lenth_sum*money[i] 
+
+
+print(result_money)
+
+
+
+
+'''
+9
+1 1 1 1 1 1 1 1
+4 3 2 7 1 7 6 2 8
+
+[4]
+[4, 2, 1]
+
+'''
