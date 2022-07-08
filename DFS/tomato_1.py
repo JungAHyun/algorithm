e99840069b979bca896b6f6bf4cb8fab70196c27
JungAHyun <66303292+JungAHyun@users.noreply.github.com>
2022-07-08 11:50:14 +0900

    upload

--- DFS/tomato_1.py
+++ DFS/tomato_1.py
@@ -2,37 +2,44 @@ from collections import deque
 import sys
 sys.setrecursionlimit(10 ** 6)  # 재귀 허용 깊이를 수동으로 늘려주는 코드
 
-def dfs(y,x):
-    if x <= -1 or x >= width or y <= -1 or y >= length: # 범위에 벗어난다면 제외
-        return False
-    if graph[y][x] == 1: # 배추가 심어진 부분이라면
-        graph[y][x] = 0 # 방문처리
-        # 상하좌우로 탐색
-        dfs(y-1, x)
-        dfs(y, x-1)
-        dfs(y+1,x)
-        dfs(y,x+1)
-        return True
-    return False
+def bfs():
+    while q:
+        x, y = q.popleft()
+        
+        for i in range(4):
+            x2 = dx[i] + x
+            y2 = dy[i] + y
+
+            if 0 <= x2 and x2 < width and 0 <= y2 and y2 < length and graph[x2][y2] == 0:
+
+                graph[x2][y2] = graph[x][y] + 1
+                q.append([x2, y2])
 
 
-test = int(input())
-result = []
-for __ in range(test):
-    width, length, position = map(int, input().split(' '))
-    graph = [[0 for _ in range(width)]] * length
+width, length = map(int, input().split(' '))
+graph = []
+cnt = 0
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]              #방향 리스트
+q = deque([])
 
-    for _ in range(position):
-        x,y = map(int, input().split(' '))
-        graph[y][x] = 1  #배추있음읕 표시
+for i in range(length):
+    tomato = list(map(int, input().split(' ')))
+    graph.append(tomato)
 
-        cnt = 0
+#토마토 있는 위치를 큐에 저장
+for i in range(length):
+    for j in range(width):
+        if  graph[i][j] == 1:   
+            q.append([i , j])
 
-        for i in range(length):
-            for j in range(width):
-                if dfs(i, j)  == 1:   
-                    cnt+=1
-    result.append(cnt)
-    
-for i in result:
-    print(i)
+bfs()
+for i in graph:
+    for j in i:
+        # 다 찾아봤는데 토마토를 익히지 못했다면 -1 출력
+        if j == 0:
+            print(-1)
+            exit(0) 
+    cnt = max(cnt, max(i))
+
+# 처음 시작을 1로 표현했으니 1을 빼준다.
+print(cnt - 1)
