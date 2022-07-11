910558728e7e5aec15fee8a8b3771416f3ef21fd
JungAHyun <66303292+JungAHyun@users.noreply.github.com>
2022-07-11 13:09:41 +0900

    upload

--- dfs_bfs/tomato_2.py
+++ dfs_bfs/tomato_2.py
@@ -12,13 +12,13 @@ def bfs():
             y2 = y + dy[i] 
 
             if (0 <= x2 and x2 < width) and (0 <= y2 and y2 < length) and (0 <= z2 and z2 < height) and graph3D[z2][y2][x2] == 0:
-                graph3D[z2][y2][x2] = graph3D[z2][y2][x2] + 1
+                graph3D[z2][y2][x2] = graph3D[z][y][x] + 1
                 q.append([z2,y2, x2])
 
 
 width, length, height = map(int, input().split(' '))
 graph3D= [[] for _ in range(height)]
-max = 0
+cnt = 0
 dx, dy, dz= [-1, 1, 0, 0, 0, 0], [0, 0, -1, 1, 0, 0], [0, 0, 0, 0, -1, 1]             #방향 리스트
 q = deque()
 
@@ -39,13 +39,14 @@ for z in range(height):
                 q.append([z,y,x])
 
 bfs()
-for lst2D in graph3D:
-    for lst in lst2D:
-        for i in lst:
-            if i==0 :
-                print(-1)
-                exit(0) 
-            if i > max:
-                max = i
 
-print(max - 1)
+for i in graph3D:
+    for j in i:
+        for k in j:
+            if k == 0:
+                print(-1)
+                exit(0)
+        cnt = max(cnt, max(j))
+        print(cnt)
+
+print(cnt - 1)
