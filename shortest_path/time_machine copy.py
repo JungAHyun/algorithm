2b6642ef84d32843c901bdf7294083b15c09876c
JungAHyun <66303292+JungAHyun@users.noreply.github.com>
2022-07-29 15:26:32 +0900

    Update time_machine copy.py

--- shortest_path/time_machine copy.py
+++ shortest_path/time_machine copy.py
@@ -4,15 +4,13 @@ input = sys.stdin.readline
 INF = int(1e9)
 n,m = map(int, input().split(' ')) #n:마을 m: 도로
 
-dist = [[INF] for _ in range(n)]    #최단거리 테이블
-edge =  [[] * 3 for _ in range(n)]  #간선의 정보를 저장하는 리스트
+dist = [INF] * (n+1)    #최단거리 테이블
+edge =  [] #간선의 정보를 저장하는 리스트
 
 for i in range(m):
     a, b, c  = map(int, input().split(' '))
-    edge[i][0].append(a)
-    edge[i][1].append(b)
-    edge[i][2].append(c)
-    
+    edge.append((a,b,c))
+
 
 def bf(start):
     dist[start] = 0
@@ -28,7 +26,7 @@ def bf(start):
                 if i == n-1:
                     return True
             
-            return False
+    return False
     
 
 negative_cycle = bf(1)
@@ -41,4 +39,4 @@ else:
         if dist[i] == INF: 
             print('-1')
         else:
-            print(dist[i])
+            print(dist[i]) 
